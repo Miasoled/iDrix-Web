ca625058e5fd16ff7e668b0fb52b3cd5fc3bcc74
Miasoled <lissethgetial@gmail.com>
2026-03-31 12:26:44 -0500

    Configuracion de rutas

---
+++ app/servicios/inteligencia-artificial/page.tsx
@@ -0,0 +1,9 @@
+import { Navbar } from "@/components/Navbar";
+
+export default function IA() {
+  return (
+    <main className="relative min-h-screen w-full overflow-hidden">
+      <Navbar />
+    </main>
+  );
+}
---
+++ app/servicios/tomatizaciones/page.tsx
@@ -0,0 +1,9 @@
+import { Navbar } from "@/components/Navbar";
+
+export default function Automatizaciones() {
+  return (
+    <main className="relative min-h-screen w-full overflow-hidden">
+      <Navbar />
+    </main>
+  );
+}
